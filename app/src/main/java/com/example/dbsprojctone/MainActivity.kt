e10e03948de664f5fb5123b755de0491175dd24f
ansari <makansari.android@gmail.com>
2022-11-24 12:06:58 +0530

    notifications

--- app/src/main/java/com/example/dbsprojctone/MainActivity.kt
+++ app/src/main/java/com/example/dbsprojctone/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.dbsprojctone
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -14,6 +20,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
@@ -80,12 +87,35 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+         val channelId = "notifications"
+         val description = "Test notification"
+        lateinit var notificationManager: NotificationManager
+        lateinit var notificationChannel: NotificationChannel
+        lateinit var notificationBuilder: Notification.Builder
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        var myIntent = Intent(this, SecondActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
         var myid = item.itemId
 
         if(myid == R.id.mytoast){
             Toast.makeText(this, "TOAST SELECTED", Toast.LENGTH_SHORT).show()
+            if(Build.VERSION.SDK_INT >= 26){
+                notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+                notificationManager.createNotificationChannel(notificationChannel)
+                notificationBuilder = Notification.Builder(this, channelId)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentText("hello ansari how r u ?")
+                    .setContentTitle("ANS")
+
+                    .setContentIntent(pendingIntent)
+            }
+
+            notificationManager.notify(1234, notificationBuilder.build())
+
         }
 
         if(myid == R.id.mysearch){
